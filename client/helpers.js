7cbc5100637d634ba68c36afb3f6af74d7f23afa
thomas <thethc3@gmail.com>
2015-03-18 12:06:09 -0400

    trying out oauth images

--- client/helpers.js
+++ client/helpers.js
@@ -10,10 +10,10 @@ Template.registerHelper('profileImage', function(userId) {
     var user = Meteor.users.findOne({_id: userId});
     var imageUrl = "https://placehold.it/150x150";
 
-    if (user.services == "twitter") {
+    if (user.services.twitter) {
         imageUrl = user.services.twitter.profile_image_url_https;
     }
-    else if (user.services == "google"){
+    else if (user.services.google){
         imageUrl = user.services.google.picture;
     }
 
