98c059ee9c7e2155b51eb4c6bca26c5033ba2167
thomas <thethc3@gmail.com>
2015-03-18 11:58:31 -0400

    trying out oauth images

--- client/helpers.js
+++ client/helpers.js
@@ -6,6 +6,20 @@ Template.registerHelper('formatDateTime', function(date) {
     return date.toLocaleDateString() + " " + date.toLocaleTimeString();
 });
 
+Template.registerHelper('profileImage', function(userId) {
+    var user = Meteor.users.findOne({_id: userId});
+    var imageUrl = "https://placehold.it/150x150";
+
+    if (user.services == "twitter") {
+        imageUrl = user.services.twitter.profile_image_url_https;
+    }
+    else if (user.services == "google"){
+        imageUrl = user.services.google.picture;
+    }
+
+    return imageUrl;
+});
+
 Template.menu.helpers({
     chatUnread: function() {
         if (Chats.find({archive: false}).count() < 1) {
